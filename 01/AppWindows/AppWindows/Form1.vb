fec3da6cabed700d456ea1e0804415b7b453bc45
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-26 18:03:12 +0100

    Adds AppWindows project

---
+++ 01/AppWindows/AppWindows/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' TODO Actualizar la implementación de la clase
+        MessageBox.Show("Hola " & TextBox1.Text)
+    End Sub
+End Class
