e6501f1f8e61762608308e7873789a91f2c56409
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-29 18:21:33 +0100

    Adds Matrices project

---
+++ 03/Matrices/Matrices/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim ListaDatos() As Integer = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+        Dim Temperaturas(365, 23, 51) As Byte
+
+        Console.WriteLine(Temperaturas.Length)
+    End Sub
+
+End Module
