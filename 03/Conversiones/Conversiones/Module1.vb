bfcb1dad41403bf4d1c3dac7859d032034c4c955
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-29 18:19:37 +0100

    Adds Conversiones project

---
+++ 03/Conversiones/Conversiones/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Cadena1, Cadena2 As String
+        Cadena1 = "10"
+        Cadena2 = "2"
+
+        Cadena1 = Cadena1 + Cadena2
+        Console.WriteLine(Cadena1)
+
+        Dim Numero As Integer, Cadena ', Cadena2 As String
+        Cadena = "5"
+        Cadena2 = "10"
+        Numero = Cadena + Cadena2
+        Numero = CInt(Cadena) + CInt(Cadena2)
+        Console.WriteLine(Numero)
+
+    End Sub
+
+End Module
