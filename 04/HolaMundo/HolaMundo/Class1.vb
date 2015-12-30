c5cf5015315326e983c5fd17368fd848775a158a
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-30 17:36:55 +0100

    Adds HolaMundo project

---
+++ 04/HolaMundo/HolaMundo/Class1.vb
@@ -0,0 +1,23 @@
+﻿Namespace ClasesAnaya
+   ' ClasesAnaya es el espacio superior
+
+   Namespace ManualVS2012
+      ' ManualVS2012 es un espacio interior
+      Public Class HolaMundo
+         ' con una clase llamada HolaMundo
+         Public Function Saludo() As String
+            Return "Hola desde el ámbito Anaya.ManualVS2012"
+         End Function
+      End Class
+   End Namespace
+
+   Namespace ManualVB2012
+      ' ManualVB2012 es otro espacio interior
+      Public Class HolaMundo
+         ' también tiene una clase HolaMundo
+         Public Function Saludo() As String
+            Return "Hola desde el ámbito Anaya.ManualVB2012"
+         End Function
+      End Class
+   End Namespace
+End Namespace
