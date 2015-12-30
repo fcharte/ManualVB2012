73ca5674820ed42b1e861aa101624d9e117a174f
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-30 17:35:44 +0100

    Adds EspaciosNombres project

---
+++ 04/EspaciosNombres/EspaciosNombres/Module1.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Console
+
+Namespace Anaya.ManualVB
+    Class HolaMundo
+        Public Shared Sub Main()
+            WriteLine("Hola desde Anaya.ManualVB")
+        End Sub
+    End Class
+End Namespace
