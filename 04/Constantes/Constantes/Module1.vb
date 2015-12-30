02d1c27c4c36e812e0653513590e336732d0b730
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-30 17:34:05 +0100

    Adds Contantes project

---
+++ 04/Constantes/Constantes/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+
+Namespace Anaya.ManualVB2012.Constantes
+
+   Class Aplicacion
+
+      Const SegundosMinuto As Integer = 60
+      Const MinutosHora As Integer = 60
+      Const HorasDia As Integer = 24
+
+      Shared Sub Main()
+         Console.WriteLine("Un día tiene {0} segundos",
+          SegundosMinuto * MinutosHora * HorasDia)
+      End Sub
+   End Class
+End Namespace
