8c49180563f4e5edd957f49c282449c9618cc76c
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-01 18:15:12 +0100

    Adds UsaHolaMundo project

---
+++ 04/UsaHolaMundo/UsaHolaMundo/Module1.vb
@@ -0,0 +1,14 @@
+﻿Imports System
+' Incluimos referencia a los dos ámbitos
+Imports VS = ClasesAnaya.ManualVS2012
+Imports VB = ClasesAnaya.ManualVB2012
+
+Module Module1
+
+   Sub Main()
+      Dim MiHolaMundo As VB.HolaMundo = New VB.HolaMundo()
+
+      Console.WriteLine(MiHolaMundo.Saludo())
+   End Sub
+
+End Module
