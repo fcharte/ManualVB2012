dacf73f13ace6e32f1d2f054b20530d5b6b61020
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-02 17:53:08 +0100

    Adds TiposGenericos Restricciones project

---
+++ 05/TiposGenericos/Restricciones/Factoria.vb
@@ -0,0 +1,39 @@
+﻿Public Class Factoria(Of TipoObjeto As {New, Control})
+   ' Para conservar los objetos creados
+   Private Lista As Hashtable = New Hashtable
+
+   ' Este método crea un nuevo objeto del tipo adecuado
+   ' asociándole una clave
+   Public Function Nuevo(ByVal Clave As String) As TipoObjeto
+      Dim Objeto = New TipoObjeto
+      Lista.Add(Clave, Objeto)
+      Return Objeto
+   End Function
+
+   ' Propiedad sólo de lectura que devuelve el objeto solicitado
+   Public ReadOnly Property Elemento(ByVal Clave As String) As TipoObjeto
+      Get
+         Return Lista(Clave)
+      End Get
+   End Property
+End Class
+
+Public Class Factoria(Of TipoClave, TipoObjeto As {New, Control})
+   ' Para conservar los objetos creados
+   Private Lista As Hashtable = New Hashtable
+
+   ' Este método crea un nuevo objeto del tipo adecuado
+   ' asociándole una clave
+   Public Function Nuevo(Clave As TipoClave) As TipoObjeto
+      Dim Objeto = New TipoObjeto
+      Lista.Add(Clave, Objeto)
+      Return Objeto
+   End Function
+
+   ' Propiedad sólo de lectura que devuelve el objeto solicitado
+   Public ReadOnly Property Elemento(Clave As TipoClave) As TipoObjeto
+      Get
+         Return Lista(Clave)
+      End Get
+   End Property
+End Class
---
+++ 05/TiposGenericos/Restricciones/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+   Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+      Dim MiFactoria As Factoria(Of String, Button) = New Factoria(Of String, Button)
+
+      MiFactoria.Nuevo("Boton1").Text = "Primer botón"
+      MiFactoria.Elemento("Boton1").Parent = Me
+      MiFactoria.Elemento("Boton1").Show()
+
+      Dim Boton As Button = MiFactoria.Nuevo("Boton2")
+      Boton.Parent = Me
+      Boton.Top = 100
+      Boton.Text = "Botón 2"
+      Boton.Show()
+
+   End Sub
+End Class
