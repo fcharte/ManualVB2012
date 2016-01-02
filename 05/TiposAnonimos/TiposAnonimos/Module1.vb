97fad6a68535e65379ec17665968092267b3edd0
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-02 17:50:36 +0100

    Adds TiposAnonimos project

---
+++ 05/TiposAnonimos/TiposAnonimos/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+      Dim Anotacion = New With {
+      .X = 17, .Y = 24, .Texto = "Puerto del aire"}
+
+      Console.WriteLine(Anotacion.Texto)
+      Console.WriteLine(Anotacion.GetType().ToString)
+    End Sub
+
+End Module
