68f3b37bf47c0982d1c826b7b6839cb554507d5e
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-02 17:50:04 +0100

    Adds MetodosExtension project

---
+++ 05/MetodosExtension/MetodosExtension/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+   <System.Runtime.CompilerServices.Extension()>
+   Public Function EsPar(ByVal N As Integer) As Boolean
+      Return N Mod 2 = 0
+   End Function
+
+   Sub Main()
+      Dim Numero = 4
+
+      Console.WriteLine(Numero.
+   End Sub
+End Module
