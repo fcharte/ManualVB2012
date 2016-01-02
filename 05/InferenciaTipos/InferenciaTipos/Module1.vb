158258daf865b2d99e0d976fdd491dfc90e0e770
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-02 17:48:15 +0100

    Adds InferenciaTipos project

---
+++ 05/InferenciaTipos/InferenciaTipos/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+   Sub Main()
+      Dim Valores() = {30, 15, 3.14, 7}
+      Dim Unidades = My.Computer.FileSystem.Drives
+
+      For Each Unidad In Unidades
+         Console.WriteLine(Unidad.DriveType.ToString())
+      Next
+
+   End Sub
+End Module
