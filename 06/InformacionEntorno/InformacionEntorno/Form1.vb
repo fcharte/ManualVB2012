de70e932677c2d8c2c97893c964dc6930d684170
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-04 17:29:51 +0100

    Adds InformacionEntorno project

---
+++ 06/InformacionEntorno/InformacionEntorno/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+   Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+      ' Matriz con los datos a recuperar
+
+      Dim Datos() = {
+      "StartupPath= " + Application.StartupPath,
+      "ExecutablePath= " + Application.ExecutablePath,
+      "CompanyName= " + Application.CompanyName,
+      "ProductName= " + Application.ProductName,
+      "ProductVersion= " + Application.ProductVersion,
+      "UserAppDataPath= " + Application.UserAppDataPath,
+      "CommonAppDataPath= " + Application.CommonAppDataPath()
+  }
+
+      ' Recorremos la matriz añadiendo los datos 
+      ' a la lista
+      For Each Cadena As String In Datos
+         lbDatos.Items.Add(Cadena)
+      Next
+
+   End Sub
+End Class
