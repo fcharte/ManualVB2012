9dc58abd58fe04011ef1d88261636b500db750a1
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-04 17:28:40 +0100

    Adds AppWPF project

---
+++ 06/AppWPF/AppWPF/MainWindow.xaml.vb
@@ -0,0 +1,8 @@
+﻿Class MainWindow 
+
+   Private Sub Button1_Click(sender As Object, e As RoutedEventArgs) Handles Button1.Click
+      ListBox1.Items.Add(TextBox1.Text)
+      TextBox1.SelectAll()
+
+   End Sub
+End Class
