c4640459c6352ed53665d482b528768c3c5ff065
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-05 18:08:42 +0100

    Adds MyConsola project

---
+++ 07/MyConsola/MyConsola/Module1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.IO
+Module Module1
+
+   Sub Main()
+      Dim Archivo As StreamReader
+      Dim Texto As String
+
+      For Each Parametro As String In My.Application.CommandLineArgs
+         ' Analizar parámetro y actuar en consecuencia
+         Console.WriteLine(Parametro)
+
+      Next
+
+      Archivo = File.OpenText("Texto.txt")
+      Texto = Archivo.ReadToEnd
+      Archivo.Close()
+
+      Texto = My.Computer.FileSystem.ReadAllText("Texto.txt")
+
+   End Sub
+
+End Module
