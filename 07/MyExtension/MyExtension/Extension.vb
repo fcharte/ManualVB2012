6b5ef9ac0ecbeb828427957cea385e3f9e88375e
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-05 18:09:11 +0100

    Adds MyExtension project

---
+++ 07/MyExtension/MyExtension/Extension.vb
@@ -0,0 +1,13 @@
+﻿Namespace My
+   Public Class Utilities
+      Private Shared strPropiedad As String
+      Public Shared Property Propiedad() As String
+         Get
+            Return strPropiedad
+         End Get
+         Set(ByVal value As String)
+            strPropiedad = value
+         End Set
+      End Property
+   End Class
+End Namespace
