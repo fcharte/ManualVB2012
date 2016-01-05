f43bfabf863038be831b0336797d1615a47e7335
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-05 18:09:48 +0100

    Adds MyWindows AccesoRegistro project

---
+++ 07/MyWindows/AccesoAlRegistro/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+   Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+      Dim Clave = My.Computer.Registry.LocalMachine.OpenSubKey(
+           "HARDWARE\DESCRIPTION\System\CentralProcessor\0")
+
+      For Each Nombre As String In Clave.GetValueNames
+         Try
+            ListBox1.Items.Add(Nombre & ": " & Clave.GetValue(Nombre).ToString)
+         Catch ex As Exception
+            Continue For
+         End Try
+      Next
+
+   End Sub
+End Class
