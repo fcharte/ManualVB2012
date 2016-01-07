8e942e8794b7180423cb7dfd9d4cdfc7f8c20f10
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-07 11:50:08 +0100

    Adds MyWindows Recursos project

---
+++ 07/MyWindows/Recursos/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+      Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+      Me.SuspendLayout()
+      '
+      'CheckBox1
+      '
+      Me.CheckBox1.AutoSize = True
+      Me.CheckBox1.Location = New System.Drawing.Point(29, 38)
+      Me.CheckBox1.Name = "CheckBox1"
+      Me.CheckBox1.Size = New System.Drawing.Size(127, 17)
+      Me.CheckBox1.TabIndex = 3
+      Me.CheckBox1.Text = "Con imagen de fondo"
+      '
+      'Form1
+      '
+      Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+      Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+      Me.ClientSize = New System.Drawing.Size(284, 261)
+      Me.Controls.Add(Me.CheckBox1)
+      Me.Name = "Form1"
+      Me.Text = "Form1"
+      Me.ResumeLayout(False)
+      Me.PerformLayout()
+
+   End Sub
+   Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ 07/MyWindows/Recursos/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+   Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+      If My.Settings.ImagenFondo Then
+         Me.BackgroundImage = My.Resources.LogoAnaya
+         CheckBox1.Checked = True
+      End If
+      Me.Text = My.Resources.Titulo
+
+   End Sub
+   Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+      My.Settings.ImagenFondo = CheckBox1.Checked
+      If My.Settings.ImagenFondo Then
+         Me.BackgroundImage = My.Resources.LogoAnaya
+      Else
+         Me.BackgroundImage = Nothing
+      End If
+   End Sub
+End Class
