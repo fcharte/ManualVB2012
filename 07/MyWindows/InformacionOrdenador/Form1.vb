f6a6c2fa617982adca9b06486428829483d0ae65
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-07 11:48:27 +0100

    Adds MyWindows InformacionOrdenador project

---
+++ 07/MyWindows/InformacionOrdenador/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+   Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+      With My.Computer
+         Dim Datos = Split(
+             "Memoria física total: " &
+             Format(.Info.TotalPhysicalMemory, "#,#") & " bytes," &
+             "Memoria física disponible: " &
+             Format(.Info.AvailablePhysicalMemory, "#,#") & " bytes," &
+             "Memoria virtual total: " &
+             Format(.Info.TotalVirtualMemory, "#,#") & " bytes," &
+             "Memoria virtual disponible: " &
+             Format(.Info.AvailableVirtualMemory, "#,#") & " bytes," &
+             "Configuración regional: " &
+             .Info.InstalledUICulture.DisplayName & "," &
+             "Sistema operativo: " & .Info.OSFullName & "," &
+             "Plataforma: " & .Info.OSPlatform & "," &
+             "Versión del sistema: " & .Info.OSVersion & ",", ",")
+
+         For Each Cadena As String In Datos
+            ListBox1.Items.Add(Cadena)
+         Next
+      End With
+
+   End Sub
+End Class
