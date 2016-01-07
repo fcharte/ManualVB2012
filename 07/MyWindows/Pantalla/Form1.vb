01094f2d9d4a08429caf4c914efc7fc01ead139e
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-07 11:49:39 +0100

    Adds MyWindows Pantalla project

---
+++ 07/MyWindows/Pantalla/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+   Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+      With My.Computer.Screen
+         ListBox1.Items.Add("Nombre: " & .DeviceName)
+         ListBox1.Items.Add("Coordenadas: " & .Bounds.ToString)
+         ListBox1.Items.Add("Área disponible: " & .WorkingArea.ToString)
+         ListBox1.Items.Add("Bits de color: " & .BitsPerPixel)
+      End With
+         My.Computer.Registry.
+   End Sub
+End Class
