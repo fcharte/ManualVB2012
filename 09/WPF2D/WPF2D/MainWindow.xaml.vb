787b94a9b13904449cda5a554bb0e94664890b71
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-10 18:30:19 +0100

    WPF2D main window

---
+++ 09/WPF2D/WPF2D/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow 
+
+   Private Sub Ellipse1_MouseUp(sender As Object, e As MouseButtonEventArgs) Handles Ellipse1.MouseUp
+      Ellipse1.Fill.Opacity *= 0.9
+      Ellipse1.RenderTransformOrigin = New Point(0.5, 0.5)
+      Ellipse1.RenderTransform = New RotateTransform(45)
+
+   End Sub
+End Class
