6be84da6d89a73258225efb81cdfafcad9b1d40b
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-13 20:38:16 +0100

    Adds WebSimple project

---
+++ 12/WebSimple/Default.aspx.vb
@@ -0,0 +1,11 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+   Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+      For N = 1 To 5
+         Response.Write("<h" & N & ">Hola mundo</h" & N & ">")
+      Next
+
+   End Sub
+End Class
