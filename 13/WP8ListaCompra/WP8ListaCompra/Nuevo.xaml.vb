342bb52fa66ddf31d39ade99adc9a3bf91026fab
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-14 18:04:47 +0100

    WP9ListaCompra project Nuevo page

---
+++ 13/WP8ListaCompra/WP8ListaCompra/Nuevo.xaml.vb
@@ -0,0 +1,16 @@
+﻿Partial Public Class Nuevo
+    Inherits PhoneApplicationPage
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+   Private Sub ApplicationBarIconButton_Click_1(sender As Object, e As EventArgs)
+      NavigationService.GoBack()
+   End Sub
+
+   Private Sub ApplicationBarIconButton_Click_2(sender As Object, e As EventArgs)
+      App.ViewModel.Items.Add(New ItemViewModel() With {.LineOne = Titulo.Text, .LineTwo = Descripción.Text, .Comprado = False})
+      NavigationService.GoBack()
+   End Sub
+End Class
