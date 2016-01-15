5516551593db8574f6cc03abbd9e1d201f81f6d7
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-15 19:54:31 +0100

    Adds Compresion project

---
+++ 14/Compresion/Compresion/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+      Me.lbTamano = New System.Windows.Forms.Label()
+      Me.btnDescomprimir = New System.Windows.Forms.Button()
+      Me.btnComprimir = New System.Windows.Forms.Button()
+      Me.Label3 = New System.Windows.Forms.Label()
+      Me.lbArchivos = New System.Windows.Forms.ListBox()
+      Me.Label2 = New System.Windows.Forms.Label()
+      Me.lbCarpetas = New System.Windows.Forms.ListBox()
+      Me.cbUnidades = New System.Windows.Forms.ComboBox()
+      Me.Label1 = New System.Windows.Forms.Label()
+      Me.SuspendLayout()
+      '
+      'lbTamano
+      '
+      Me.lbTamano.AutoSize = True
+      Me.lbTamano.Location = New System.Drawing.Point(207, 206)
+      Me.lbTamano.Name = "lbTamano"
+      Me.lbTamano.Size = New System.Drawing.Size(41, 13)
+      Me.lbTamano.TabIndex = 42
+      Me.lbTamano.Text = "0 bytes"
+      '
+      'btnDescomprimir
+      '
+      Me.btnDescomprimir.Location = New System.Drawing.Point(309, 239)
+      Me.btnDescomprimir.Name = "btnDescomprimir"
+      Me.btnDescomprimir.Size = New System.Drawing.Size(86, 23)
+      Me.btnDescomprimir.TabIndex = 41
+      Me.btnDescomprimir.Text = "Descomprimir"
+      '
+      'btnComprimir
+      '
+      Me.btnComprimir.Location = New System.Drawing.Point(207, 238)
+      Me.btnComprimir.Name = "btnComprimir"
+      Me.btnComprimir.Size = New System.Drawing.Size(75, 23)
+      Me.btnComprimir.TabIndex = 40
+      Me.btnComprimir.Text = "Comprimir"
+      '
+      'Label3
+      '
+      Me.Label3.AutoSize = True
+      Me.Label3.Location = New System.Drawing.Point(208, 18)
+      Me.Label3.Name = "Label3"
+      Me.Label3.Size = New System.Drawing.Size(113, 13)
+      Me.Label3.TabIndex = 35
+      Me.Label3.Text = "Seleccione un archivo"
+      '
+      'lbArchivos
+      '
+      Me.lbArchivos.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+      Me.lbArchivos.FormattingEnabled = True
+      Me.lbArchivos.Location = New System.Drawing.Point(208, 34)
+      Me.lbArchivos.Name = "lbArchivos"
+      Me.lbArchivos.Size = New System.Drawing.Size(279, 160)
+      Me.lbArchivos.TabIndex = 39
+      '
+      'Label2
+      '
+      Me.Label2.AutoSize = True
+      Me.Label2.Location = New System.Drawing.Point(16, 66)
+      Me.Label2.Name = "Label2"
+      Me.Label2.Size = New System.Drawing.Size(120, 13)
+      Me.Label2.TabIndex = 34
+      Me.Label2.Text = "Seleccione una carpeta"
+      '
+      'lbCarpetas
+      '
+      Me.lbCarpetas.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+      Me.lbCarpetas.FormattingEnabled = True
+      Me.lbCarpetas.Location = New System.Drawing.Point(16, 82)
+      Me.lbCarpetas.Name = "lbCarpetas"
+      Me.lbCarpetas.Size = New System.Drawing.Size(160, 186)
+      Me.lbCarpetas.TabIndex = 38
+      '
+      'cbUnidades
+      '
+      Me.cbUnidades.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+      Me.cbUnidades.DropDownWidth = 120
+      Me.cbUnidades.FormattingEnabled = True
+      Me.cbUnidades.Location = New System.Drawing.Point(16, 34)
+      Me.cbUnidades.Name = "cbUnidades"
+      Me.cbUnidades.Size = New System.Drawing.Size(160, 21)
+      Me.cbUnidades.TabIndex = 37
+      '
+      'Label1
+      '
+      Me.Label1.AutoSize = True
+      Me.Label1.Location = New System.Drawing.Point(16, 18)
+      Me.Label1.Name = "Label1"
+      Me.Label1.Size = New System.Drawing.Size(116, 13)
+      Me.Label1.TabIndex = 36
+      Me.Label1.Text = "Seleccione una unidad"
+      '
+      'Form1
+      '
+      Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+      Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+      Me.ClientSize = New System.Drawing.Size(508, 279)
+      Me.Controls.Add(Me.lbTamano)
+      Me.Controls.Add(Me.btnDescomprimir)
+      Me.Controls.Add(Me.btnComprimir)
+      Me.Controls.Add(Me.Label3)
+      Me.Controls.Add(Me.lbArchivos)
+      Me.Controls.Add(Me.Label2)
+      Me.Controls.Add(Me.lbCarpetas)
+      Me.Controls.Add(Me.cbUnidades)
+      Me.Controls.Add(Me.Label1)
+      Me.Name = "Form1"
+      Me.Text = "Compresión de archivos"
+      Me.ResumeLayout(False)
+      Me.PerformLayout()
+
+   End Sub
+   Friend WithEvents lbTamano As System.Windows.Forms.Label
+   Friend WithEvents btnDescomprimir As System.Windows.Forms.Button
+   Friend WithEvents btnComprimir As System.Windows.Forms.Button
+   Friend WithEvents Label3 As System.Windows.Forms.Label
+   Friend WithEvents lbArchivos As System.Windows.Forms.ListBox
+   Friend WithEvents Label2 As System.Windows.Forms.Label
+   Friend WithEvents lbCarpetas As System.Windows.Forms.ListBox
+   Friend WithEvents cbUnidades As System.Windows.Forms.ComboBox
+   Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ 14/Compresion/Compresion/Form1.vb
@@ -0,0 +1,116 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+
+Public Class Form1
+
+   Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+      For Each Unidad In My.Computer.FileSystem.Drives
+         If Unidad.IsReady Then
+            cbUnidades.Items.Add(Unidad.Name & " [" & Unidad.VolumeLabel & "]")
+         Else
+            cbUnidades.Items.Add(Unidad.Name)
+         End If
+
+         If cbUnidades.SelectedIndex = -1 AndAlso
+             Unidad.DriveType = DriveType.Fixed Then
+            cbUnidades.SelectedIndex = cbUnidades.Items.Count - 1
+         End If
+      Next
+   End Sub
+
+   Private Sub cbUnidades_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbUnidades.SelectedIndexChanged
+      lbCarpetas.Items.Clear()
+
+      For Each Carpeta In My.Computer.FileSystem.GetDirectories(
+            Mid(cbUnidades.Text, 1, 3))
+         lbCarpetas.Items.Add(Path.GetFileName(Carpeta))
+      Next
+      lbCarpetas.SelectedIndex = 0
+   End Sub
+
+   Private Sub lbCarpetas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbCarpetas.SelectedIndexChanged
+      lbArchivos.Items.Clear()
+      Try
+         For Each Archivo In My.Computer.FileSystem.GetFiles(
+             Mid(cbUnidades.Text, 1, 3) & lbCarpetas.Text,
+             FileIO.SearchOption.SearchTopLevelOnly, "*.*")
+            lbArchivos.Items.Add(Path.GetFileName(Archivo))
+         Next
+      Catch
+      End Try
+   End Sub
+
+   Private Sub lbArchivos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbArchivos.SelectedIndexChanged
+      Dim NombreArchivo =
+       Mid(cbUnidades.Text, 1, 3) & lbCarpetas.Text & "\" &
+       lbArchivos.Text
+
+      Dim Inf As New FileInfo(NombreArchivo)
+
+      ' Mostramos el número de bytes de longitud
+      lbTamano.Text = Inf.Length & " bytes"
+
+      ' Activamos o desactivamos los botones según la extensión del archivo
+      btnComprimir.Enabled = Path.GetExtension(lbArchivos.Text) <> ".gz"
+      btnDescomprimir.Enabled = Path.GetExtension(lbArchivos.Text) = ".gz"
+   End Sub
+
+   Private Sub btnComprimir_Click(sender As Object, e As EventArgs) Handles btnComprimir.Click
+      ' Componemos el nombre del archivo
+      Dim NombreArchivo = Mid(cbUnidades.Text, 1, 3) & lbCarpetas.Text & "\" &
+       lbArchivos.Text
+      ' y obtenemos información sobre él
+      Dim Inf As New FileInfo(NombreArchivo)
+
+      ' Leemos todo el contenido del archivo en una matriz
+      Dim Contenido(CInt(Inf.Length)) As Byte
+      Contenido = My.Computer.FileSystem.ReadAllBytes(NombreArchivo)
+
+      ' Creamos un archivo donde escribir los datos comprimidos
+      Using Salida As New FileStream(NombreArchivo & ".gz", FileMode.Create)
+         ' y el objeto asociado que los comprimirá
+         Using Compresor As New GZipStream(Salida, CompressionMode.Compress, True)
+            ' Escribimos los datos en el flujo comprimido 
+            Compresor.Write(Contenido, 0, Contenido.Length)
+            ' lo cerramos para escribir las cabeceras en el archivo
+            Compresor.Close()
+            ' mostramos el tamaño de la información comprimida
+            lbTamano.Text = "Comprimido a " & Salida.Length & " bytes"
+         End Using
+      End Using
+
+      ' Actualizamos la lista de archivos para mostrar el recién creado
+      lbCarpetas_SelectedIndexChanged(Me, System.EventArgs.Empty)
+   End Sub
+
+   Private Sub btnDescomprimir_Click(sender As Object, e As EventArgs) Handles btnDescomprimir.Click
+      ' Componemos el nombre del archivo
+      Dim NombreArchivo = Mid(cbUnidades.Text, 1, 3) & lbCarpetas.Text & "\" &
+       lbArchivos.Text
+
+      ' Creamos el objeto de descompresión para que lea del archivo
+      Using Compresor As New GZipStream(New FileStream(NombreArchivo, FileMode.Open),
+        CompressionMode.Decompress)
+         ' creamos el archivo de salida
+         Using Salida As New FileStream(Mid(cbUnidades.Text, 1, 3) &
+             lbCarpetas.Text & "\" & Path.GetFileNameWithoutExtension(NombreArchivo),
+             FileMode.Create)
+
+            Dim Contenido(256) As Byte
+            Dim BytesLeidos As Integer, Inicio As Integer = 0
+            While True ' Vamos leyendo bloques de 256 bytes
+               BytesLeidos = Compresor.Read(Contenido, 0, 256)
+               If BytesLeidos <> 0 Then
+                  ' y escribiéndolos en el archivo de saluda
+                  Salida.Write(Contenido, 0, BytesLeidos)
+               Else
+                  Exit While
+               End If
+            End While
+         End Using
+      End Using
+
+      ' Actualizamos la lista de archivos para mostrar el recién creado
+      lbCarpetas_SelectedIndexChanged(Me, System.EventArgs.Empty)
+   End Sub
+End Class
