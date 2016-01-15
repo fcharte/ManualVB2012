87ce5b30398b7690505c7f8b7c3cf2bffe76561f
Francisco Charte Ojeda <francisco@fcharte.com>
2016-01-15 19:55:08 +0100

    Adds DirectoriosSistema project

---
+++ 14/DirectoriosSistema/DirectoriosSistema/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+      With My.Computer.FileSystem.SpecialDirectories
+         Dim Datos =
+          {{"AllUsersApplicationData", .AllUsersApplicationData},
+           {"CurrentUserApplicationData", .CurrentUserApplicationData},
+           {"Desktop", .Desktop}, {"MyDocuments", .MyDocuments},
+           {"MyMusic", .MyMusic}, {"MyPictures", .MyPictures},
+           {"Programs", .Programs}, {"Temp", .Temp}}
+
+         For Indice = 0 To Datos.GetUpperBound(0)
+            Console.WriteLine(Datos(Indice, 0) & ": " & Datos(Indice, 1))
+         Next
+      End With
+
+    End Sub
+
+End Module
